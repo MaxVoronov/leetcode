575623fb9c4c0056e18becc3ba25e19566ab2ca1
Max Voronov <v0id@list.ru>
2024-01-24 20:54:46 +0200

    283. Move Zeroes (improved)

--- go/0283_move_zeroes/solution.go
+++ go/0283_move_zeroes/solution.go
@@ -5,17 +5,11 @@ func moveZeroes(nums []int) {
 		return
 	}
 
-	idx := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != 0 {
-			nums[idx] = nums[i]
-			idx++
-		}
-	}
-
-	if idx > 0 {
-		for ; idx < len(nums); idx++ {
-			nums[idx] = 0
+	var wPos int
+	for rPos := 0; rPos < len(nums); rPos++ {
+		if nums[rPos] != 0 {
+			nums[wPos], nums[rPos] = nums[rPos], nums[wPos]
+			wPos++
 		}
 	}
 }
