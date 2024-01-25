8478c422c3f76092c1b6c68791cde7ad0788888a
Max Voronov <v0id@list.ru>
2024-01-25 17:16:47 +0200

    7. Reverse Integer (improved)

--- go/0007_reverse_integer/solution.go
+++ go/0007_reverse_integer/solution.go
@@ -2,6 +2,34 @@ package reverse_integer
 
 import "math"
 
+func ReverseV2(x int) int {
+	if x >= -9 && x <= 9 {
+		return x
+	}
+
+	if x > math.MaxInt32 || x < math.MinInt32 {
+		return 0 // Int32 overflow
+	}
+
+	// Assume the environment does not allow you to store 64-bit integers by conditions
+	var result, num, mul int32
+	numLen := int(math.Log10(math.Abs(float64(x))))
+
+	for i := numLen; i >= 0; i-- {
+		num = int32(x % 10)
+		mul = int32(math.Pow10(i))
+
+		if math.MaxInt32/mul < result/mul+num || math.MinInt32/mul > result/mul+num {
+			return 0 // Int32 overflow
+		}
+
+		result += num * mul
+		x /= 10
+	}
+
+	return int(result)
+}
+
 func Reverse(x int) int {
 	if x >= -9 && x <= 9 {
 		return x
--- go/0007_reverse_integer/solution_test.go
+++ go/0007_reverse_integer/solution_test.go
@@ -1,32 +1,73 @@
 package reverse_integer
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
-func TestReverse(t *testing.T) {
-	tests := []struct {
-		input int
-		want  int
-	}{
-		{input: 123, want: 321},
-		{input: -123, want: -321},
-		{input: 120, want: 21},
-		{input: 5, want: 5},
-		{input: -7, want: -7},
-		{input: 0, want: 0},
-		// Int32 overflow
-		{input: 1_363_847_412, want: 2_147_483_631},
-		{input: 1_563_847_412, want: 0},
-		{input: 1_534_236_469, want: 0},
-		{input: 123_456_789_876, want: 0},
-	}
+var testCases = []struct {
+	input int
+	want  int
+}{
+	{input: 123, want: 321},
+	{input: -123, want: -321},
+	{input: 120, want: 21},
+	{input: 5, want: 5},
+	{input: -7, want: -7},
+	{input: 0, want: 0},
+	// Int32 overflow
+	{input: 1_363_847_412, want: 2_147_483_631},
+	{input: 1_563_847_412, want: 0},
+	{input: 1_534_236_469, want: 0},
+	{input: 123_456_789_876, want: 0},
+	{input: -2_147_483_648, want: 0},
+	{input: -123_456_789_876, want: 0},
+}
 
-	for i, tc := range tests {
+var benchCases = []struct {
+	input int
+}{
+	{input: 123},
+	{input: -123},
+	{input: 1_363_847_412},
+	{input: 1_563_847_412},
+}
+
+func TestReverse(t *testing.T) {
+	for i, tc := range testCases {
 		got := Reverse(tc.input)
 		if !reflect.DeepEqual(tc.want, got) {
 			t.Fatalf("Case #%d: expected: %v, got: %v", i+1, tc.want, got)
 		}
+	}
+}
+
+func TestReverseV2(t *testing.T) {
+	for i, tc := range testCases {
+		got := ReverseV2(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("Case #%d: expected: %v, got: %v", i+1, tc.want, got)
+		}
+	}
+}
+
+func Benchmark_Reverse(b *testing.B) {
+	for i, bc := range benchCases {
+		b.Run(fmt.Sprintf("Case_%d", i), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Reverse(bc.input)
+			}
+		})
+	}
+}
+
+func Benchmark_ReverseV2(b *testing.B) {
+	for i, bc := range benchCases {
+		b.Run(fmt.Sprintf("Case_%d", i), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ReverseV2(bc.input)
+			}
+		})
 	}
 }
